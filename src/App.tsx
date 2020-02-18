9baf302b7b886eff3d9cdf5fe804b19ec4d7215a
Guillaume Buaud <GuiGui@iMac-de-Louise.local>
2020-02-18 19:52:38 +0100

    mise à jour

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React, { FunctionComponent } from "react";
 import PokemonList from "./pages/pokemon-list";
+import PokemonDetail from "./pages/pokemon-detail";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PageNoFound from "./pages/page-not-found";
 
 const App: FunctionComponent = () => {
-  return <PokemonList />;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/:id" component={PokemonDetail} />
+          <Route component={PageNoFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
